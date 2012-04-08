a7d4b9cd6adeae2453f4ab9ee642f6ab9d588745
vertino <none@none>
2012-04-09 00:36:45 +0100

    Dropped down the version number... v0.1.4 pre-release.

--- Our.Umbraco.BackOfficePowerScripts/Properties/AssemblyInfo.cs
+++ Our.Umbraco.BackOfficePowerScripts/Properties/AssemblyInfo.cs
@@ -25,8 +25,8 @@ using Our.Umbraco.BackOfficePowerScripts.Extensions;
 // Version information for an assembly consists of the following four values:
 // Major Version, Minor Version, Build Number, Revision
 // You can specify all the values or you can default the Build and Revision Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.*")]
-// [assembly: AssemblyFileVersion("1.0.0.*")]
+[assembly: AssemblyVersion("0.1.4.*")]
+// [assembly: AssemblyFileVersion("0.1.4.*")]
 
 // register the pre-application start method(s)
 [assembly: PreApplicationStartMethod(typeof(Application), Common.PreApplicationStartMethodName)]
